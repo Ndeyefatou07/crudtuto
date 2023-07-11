0f89fb85c650f2875b94252b2ffa3b4eec3aea1e
Ndeye Fatou Niang <ndeyefatoun126@gmail.com>
2023-07-11 23:37:28 +0000

    verdion final

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
-var productdb = (dbname,table)=>{
-    const db= new Dexie(dbname);
-    db.version(1).stores(table);
-    db.open();
-    return db;
-}
-// insert function
-const bulkcreate=(dbtable,data)=>{
-   let flag= empty(data);
-   if(flag){
+
+const productsdb = (dbname, table) => {
+  const db = new Dexie(dbname);
+  db.version(1).stores(table);
+  db.open();
+
+  return db;
+ 
+};
+
+const bulkcreate = (dbtable, data) => {
+  let flag = empty(data);
+  if (flag) {
     dbtable.bulkAdd([data]);
-    console.log("les données sont enregistrés avec sucess")
-   }else{
-    console.log("remplis les données")
-   }
-   return flag;
-}
-const empty=object=>{
-    let flag=false;
-    for (const value in object){
-        if(object[value]!="" &&object.hasOwnProperty(value)){
-            flag=true;
-        }else{
-            flag=false;
-        }
-    }
-    return flag;
-}
-// get data
-const getData = (dbtable,fn) => {
-    let index = 0;
-    let obj = {};
-    dbtable.products.count((count) => {
-      if (count) {
-        dbtable.each(table => {
-         
-          obj=Sortobj(table);
-        fn(obj, index++);
-        });
-      }else{
-        fn(0);
-      }
-    });
-  };
-
-//   sort object
-  const Sortobj = sortobj =>{
-    let obj={};
-    obj={
-      id:sortobj.id,
-      name:sortobj.name,
-      seller:sortobj.seller,
-      price:sortobj.price
-    }
-    return obj;
+    console.log("Données enregistrés avec succès...!");
+  } else {
+    console.log("Remplis les données...!");
   }
+  return flag;
+};
 
-export default productdb;
-export{
-    bulkcreate,
-    getData
+// create dynamic elements
+const createEle = (tagname, appendTo, fn) => {
+  const element = document.createElement(tagname);
+  if (appendTo) appendTo.appendChild(element);
+  if (fn) fn(element);
+};
+
+// check textbox validation
+const empty = object => {
+  let flag = false;
+  for (const value in object) {
+    if (object[value] != "" && object.hasOwnProperty(value)) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+  }
+  return flag;
+};
+
+// getData from the database
+const getData = (dbname, fn) => {
+  let index = 0;
+  let obj = {};
+  dbname.count(count => {
+    // count rows in the table using count method
+    if (count) {
+      dbname.each(table => {
+        // table => return the table object data
+        // to arrange order we are going to create for in loop
+        obj = SortObj(table);
+        fn(obj, index++); // call function with data argument
+      });
+    } else {
+      fn(0);
+    }
+  });
+};
+
+const SortObj = (sortobj) => {
+  let obj = {};
+  obj = {
+    id: sortobj.id,
+    name: sortobj.name,
+    seller: sortobj.seller,
+    price: sortobj.price
+  };
+  return obj;
 }
+
+
+export default productsdb;
+export {
+  bulkcreate,
+  createEle,
+  getData,
+  SortObj
+};
